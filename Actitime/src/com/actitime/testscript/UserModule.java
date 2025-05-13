0daebf758d76a0524d4ea6d4e0eec756e88879d5
QSP <QSP@DESKTOP-3JRUFPN>
2025-05-13 11:30:58 +0530

    Created a new class for User Module

---
+++ Actitime/src/com/actitime/testscript/UserModule.java
@@ -0,0 +1,14 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.actitime.generic.BaseClass;
+
+public class UserModule extends BaseClass{
+	@Test
+public void addUser() {
+	Reporter.log("addUser",true);
+	
+}
+}
