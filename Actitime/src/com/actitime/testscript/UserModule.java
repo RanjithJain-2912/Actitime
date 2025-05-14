f1978d59234fb5d2387a6c4140f7fcdffcb3d78d
RanjithJain-2912 <ranjithsrn2912@gmail.com>
2025-05-14 13:20:09 +0530

    Added lsistener implementation to the test class

--- Actitime/src/com/actitime/testscript/UserModule.java
+++ Actitime/src/com/actitime/testscript/UserModule.java
@@ -1,10 +1,11 @@
 package com.actitime.testscript;
 
+import org.testng.annotations.Listeners;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import com.actitime.generic.BaseClass;
-
+@Listeners(com.actitime.generic.ListenerImplementation.class)
 public class UserModule extends BaseClass{
 	@Test
 public void addUser() {
